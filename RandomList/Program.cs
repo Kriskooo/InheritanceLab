9d8631bec41717dd9fad32d8074529226b27371a
Kriskooo <kris.danielov@gmail.com>
2021-02-25 17:28:22 +0200

    InheritanceLab

---
+++ RandomList/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList list = new RandomList();
+            list.Add("Krisko");
+            list.Add("Gosho");
+            list.Add("Pesho");
+            list.Add("kris");
+            Console.WriteLine(list.Count);
+            Console.WriteLine(list.RandomString());
+            Console.WriteLine(list.Count);
+
+        }
+    }
+}
